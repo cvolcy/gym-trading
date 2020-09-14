cd6c7a8c7b02a74e98e0828f7bde7e049d2218b7
Unknown <clervens.volcy@gmail.com>
2020-09-14 15:52:03 -0400

    remove deprecated method `timestep_limit`

--- gym_trading/__init__.py
+++ gym_trading/__init__.py
@@ -3,10 +3,5 @@ from gym.envs.registration import register
 register(
     id='trading-v0',
     entry_point='gym_trading.envs:TradingEnv',
-    timestep_limit=1000,
+    max_episode_steps=1000,
 )
-#register(
-#    id='foo-extrahard-v0',
-#    entry_point='gym_foo.envs:FooExtraHardEnv',
-#    timestep_limit=1000,
-#)
